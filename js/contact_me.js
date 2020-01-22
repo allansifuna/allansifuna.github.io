6d39ec1ac06621fbb24192685111dae643415dda
allansifuna <allansifuna324@gmail.com>
2020-01-22 15:37:27 +0300

    updated post link

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "http://allansifuna.herokuapp.com/portfolio",
+        url: "https://allanns.herokuapp.com/portfolio",
         type: "POST",
         data: {
           name: name,
@@ -29,19 +29,19 @@ $(function() {
           message: message
         },
         cache: false,
-        success: function() {
+        error: function() {
           // Success message
           $('#success').html("<div class='alert alert-success'>");
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>Your message has been sent. I hope to reach out to you soon </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
         },
-        error: function() {
+        success: function() {
           // Fail message
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
